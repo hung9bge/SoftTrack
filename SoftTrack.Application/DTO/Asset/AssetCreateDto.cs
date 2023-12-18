99f2b8f37cb7f9a342aa7f4db0f6a7ab22e4245d
hung9bge <ahungthanhgank@gmail.com>
2023-12-18 21:58:59 +0700

    fix bug

--- SoftTrack.Application/DTO/Asset/AssetCreateDto.cs
+++ SoftTrack.Application/DTO/Asset/AssetCreateDto.cs
@@ -8,18 +8,18 @@ namespace SoftTrack.Manage.DTO
 {
     public class AssetCreateDto
     {
-        public string Name { get; set; }
-        public string Cpu { get; set; }
-        public string Gpu { get; set; }
-        public string Ram { get; set; }
-        public string Memory { get; set; }
-        public string Os { get; set; }
-        public string Version { get; set; }
+        public string? Name { get; set; }
+        public string? Cpu { get; set; }
+        public string? Gpu { get; set; }
+        public string? Ram { get; set; }
+        public string? Memory { get; set; }
+        public string? Os { get; set; }
+        public string? Version { get; set; }
         public string IpAddress { get; set; }
-        public string Bandwidth { get; set; }
-        public string Manufacturer { get; set; }
-        public string Model { get; set; }
-        public string SerialNumber { get; set; }
+        public string? Bandwidth { get; set; }
+        public string? Manufacturer { get; set; }
+        public string? Model { get; set; }
+        public string? SerialNumber { get; set; }
         public String? LastSuccesfullScan { get; set; }
         public int Status { get; set; }
     }
--- SoftTrack.Domain/Entity/Asset.cs
+++ SoftTrack.Domain/Entity/Asset.cs
@@ -12,18 +12,18 @@ namespace SoftTrack.Domain
         }
 
         public int AssetId { get; set; }
-        public string Name { get; set; }
-        public string Cpu { get; set; }
-        public string Gpu { get; set; }
-        public string Ram { get; set; }
-        public string Memory { get; set; }
-        public string Os { get; set; }
-        public string Version { get; set; }
+        public string? Name { get; set; }
+        public string? Cpu { get; set; }
+        public string? Gpu { get; set; }
+        public string? Ram { get; set; }
+        public string? Memory { get; set; }
+        public string? Os { get; set; }
+        public string? Version { get; set; }
         public string IpAddress { get; set; }
-        public string Bandwidth { get; set; }
-        public string Manufacturer { get; set; }
-        public string Model { get; set; }
-        public string SerialNumber { get; set; }
+        public string? Bandwidth { get; set; }
+        public string? Manufacturer { get; set; }
+        public string? Model { get; set; }
+        public string? SerialNumber { get; set; }
         public DateTime? LastSuccesfullScan { get; set; }
         public int Status { get; set; }
 
